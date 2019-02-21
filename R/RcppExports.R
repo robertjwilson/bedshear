53073e6efc00b001001c674a65f198fb0f7911c2
Robert Wilson <robertwilson190@gmail.com>
2019-02-21 17:47:28 +0000

    delete shear_stress_original. This was only in the folder for testing

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 stress <- function(h, D50, mu_V, phic_V, Hs_V, Px_V, switch1, phiw_V) {
-    .Call('_bedshear_stress', PACKAGE = 'bedshear', h, D50, mu_V, phic_V, Hs_V, Px_V, switch1, phiw_V)
+    .Call(`_bedshear_stress`, h, D50, mu_V, phic_V, Hs_V, Px_V, switch1, phiw_V)
 }
 
